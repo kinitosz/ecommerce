415d8c27fca1b469ad1a5d0ba668a921e21c89fa
Guilherme Feitosa Monteiro <guinhofeitosa1@gmail.com>
2024-05-10 16:36:10 -0300

    Terminando a Estilização do Menu de Navegação

--- src/js/buscar.js
+++ src/js/buscar.js
@@ -10,3 +10,20 @@ lupa.addEventListener('click', ()=> {
 btnFechar.addEventListener('click', ()=> {
     boxBuscar.classList.remove('ativar')
 });
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 6000)
+
+function nextImage(){
+    count++;
+    if(count > 4){
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+
+}
