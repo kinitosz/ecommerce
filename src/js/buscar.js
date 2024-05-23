15186cc98ec0d712b69611c4fc3e4424bf4a4c15
Guilherme Feitosa Monteiro <guinhofeitosa1@gmail.com>
2024-05-23 17:59:54 -0300

    v0.62

--- src/js/buscar.js
+++ src/js/buscar.js
@@ -3,16 +3,44 @@ let boxBuscar = document.querySelector('.search-box');
 let lupa = document.querySelector('.lupa');
 let btnFechar = document.querySelector('.btn-fechar');
 let count = 1;
-document.getElementById("radio1").checked = true;
-const menuScroll = document.querySelector('.menu')
-const imgs = document.getElementById("img");
-const img = document.querySelectorAll("#img img");
+const menuScroll = document.querySelector('.menu');
+const slider = document.querySelectorAll('slider-img');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+let currentSlide = 0;
 
-let idx = 0;
-
-function carrossel(){
-    idx++;
+function hideSlider() {
+    slider.forEach(item => item.classList.remove('on'))
 }
+
+function showSlider() {
+    slider[currentSlide].classList.add('on')
+}
+
+function nextSlider () {
+    hideSlider()
+    if(currentSlide == slider.length -1) {
+        currentSlide = 0
+    } else {
+        currentSlide++
+    }
+    showSlider()
+}
+
+function prevSlider () {
+    hideSlider()
+    if(currentSlide == 0) {
+        currentSlide = slider.length -1
+    } else {
+        currentSlide++
+    }
+    showSlider()
+}
+
+btnNext.addEventListener('click' ())
+btnPrev.addEventListener('click' ())
+
+
 
 
 lupa.addEventListener('click', ()=> {
@@ -22,6 +50,8 @@ lupa.addEventListener('click', ()=> {
 btnFechar.addEventListener('click', ()=> {
     boxBuscar.classList.remove('ativar')
 });
+
+document.getElementById("radio1").checked = true;
 
 setInterval( function(){
     nextImage();
