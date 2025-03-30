b6305cc5bd78cab0ab7555850f3f8ecfac5ec43c
Guilherme Feitosa Monteiro <guinhofeitosa1@gmail.com>
2025-03-30 16:58:05 -0300

    v0.8
    Otimização para manipulação de CSS e JavaScript

--- src/js/script.js
+++ src/js/script.js
@@ -1,57 +1,20 @@
-
 function searchBox(){
     let boxBuscar = document.querySelector('.search-box');
     let lupa = document.querySelector('.lupa');
     let btnFechar = document.querySelector('.btn-fechar');
-
+  
     lupa.addEventListener('click', ()=> {
     boxBuscar.classList.add('ativar')
     });
-
+  
     btnFechar.addEventListener('click', ()=> {
         boxBuscar.classList.remove('ativar')
     });
-}
-searchBox()
+  }
+  searchBox()
+  
 
 
-function slideHome() {
-
-    let count = 1;
-    const radio = document.querySelectorAll('.slides input')
-    const btn = document.querySelectorAll('label[class^="manual"]')
-    
-    document.getElementById("radio1").checked = true;
-    setInterval( function(){
-        nextImage();
-        ativarImage();
-    }, 6000)
-
-    function nextImage(){
-        count++;
-        if(count > 4){
-            count = 1;
-        }
-
-        document.getElementById("radio" + count).checked = true;
-    }
-
-    radio.forEach((e) => {
-        e.addEventListener('click', ativarImage)
-    })
-
-    function ativarImage(){
-        radio.forEach((item, index) => {
-        if(item.checked){
-            btn[index].classList.add('ativo')
-        } else {
-            btn[index].classList.remove('ativo')
-        }
-        })
-        item.addEventListener('click', selImage)
-    }
-}
-slideHome()
 
 
 function menuShow() {
